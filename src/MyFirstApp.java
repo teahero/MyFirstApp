df49ba0ab890ca39eaa435b97c9d6de8da3d0a2c
teahero <fa11enan9e10ve@gmail.com>
2024-07-19 10:07:24 +0000

    Изменил вывод текста отображаемого в консоли

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
-        System.out.println("Meow!");
-        Scanner console = new Scanner(System.in, "cp866");
-        System.out.print("Введите свое имя:");
-        System.out.println(console.nextLine());
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
     }
 }
